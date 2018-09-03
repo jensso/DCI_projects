cfc0b4e5c6cb094ef8d259738e69552b57358123
jens soltwedel <jens.soltwedel@hotmail.com>
2018-09-03 20:40:39 +0200

    new js files added

---
+++ Scripts/javascripting.js
@@ -0,0 +1,21 @@
+// This is my age
+console.log(47);
+
+// This is the age  my Dad died
+console.log(66);
+
+/*this is the amount of years
+there will be until I reach this age*/
+console.log(66-47);
+
+console.log(222+111);
+console.log(222-111);
+console.log(222*111);
+console.log(222/111);
+console.log(222%111);
+
+// the number of days earths circles around the sun
+console.log(365);
+
+// the number of days mercury circles around the sun
+console.log(365*0.15);
