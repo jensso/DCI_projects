deab436efe281089eb61661bd6a558e34c8cf9fc
jens soltwedel <jens.soltwedel@hotmail.com>
2018-09-05 16:15:30 +0200

    new js exercise from jake

--- Scripts/javascripting.js
+++ Scripts/javascripting.js
@@ -1,62 +1,24 @@
-// This is my age
-console.log(47);
+/*
+var anzahlKugeln = 5;
 
-// This is the age  my Dad died
-console.log(66);
 
-/*this is the amount of years
-there will be until I reach this age*/
-console.log(66-47);
+switch (anzahlKugeln) {
+    case 1:
+    console.log(`Sie haben sich für 1 Kugel entschieden. Bitte zahlen Sie 1.00 $.`);
+    break;
+    case 2:
+    console.log(`Sie haben sich für 2 Kugeln entschieden. Bitte zahlen Sie 1.80 $.`);
+    break;
+    case 3:
+    console.log(`Sie haben sich für 3 Kugeln entschieden. Bitte zahlen Sie 2.50 $.`);
+    break;
+    case 4:
+    console.log(`Sie haben sich für 4 Kugeln entschieden. Bitte zahlen Sie 3.30 $.`);
+    break;
+    case 5:
+    console.log(`Sie haben sich für 5 Kugeln entschieden. Bitte zahlen Sie 3.30 $.`);
 
-console.log(222+111);
-console.log(222-111);
-console.log(222*111);
-console.log(222/111);
-console.log(222%111);
-
-// the number of days earths circles around the sun
-console.log(365);
-
-// the number of days mercury circles around the sun
-console.log(365*0.15);
-
-console.log('this is my first string');
-console.log('...and this my second');
-console.log(`..maybe I should create a loop.....`);
-
-console.log(`It s been ${2018-1969} years since moon-landing`);
-console.log('It s been '+(2018-1969)+' years since moon-landing');
-
-console.log(typeof(50+`35`));
-console.log(typeof(112+55-`33`));
-
-var myAge = 47;
-var dadsAge = 66+6;
-var dadBecomingDaddy = dadsAge - myAge;
-var vater = `Papa wurde Vater mit ${dadBecomingDaddy}`;
-console.log(vater);
-
-var celsiusReykjavik = 12;
-var celsiusRome = 28;
-var kelvinReykjavik = celsiusReykjavik +273;
-var kelvinRome = celsiusRome +273;
-console.log(kelvinReykjavik);
-console.log(kelvinRome);
-
-var randomNumber = 44;
-var even =  randomNumber  %2;
-
-console.log(even);
-/*(!) HOISTING : scans code before! executing regarding existence of Variables & Functions,
-this is the reason not executed code in statements is labelt as 'undefinded'*/
-var myMoney = 100;
-var hotelRate = 200;
-// assuming a suite cost double as a regular room
-if (myMoney*2 > hotelRate) {
-  console.log(`Take the suite`)
-} else if (myMoney <= hotelRate) {
-  console.log(`take the room`)
+    default:
+    console.log(`Sie haben sich für 2 Kugeln entschieden. Bitte zahlen Sie 1.80 $.`);
 }
-else {
-  console.log(`leave mate!`)
-};
+*/
---
+++ Scripts/reorder_exercise.js
@@ -0,0 +1,34 @@
+  var randomNumber = 778;
+  var jackpot = 1000;
+  var standardWinnings = 10;
+
+  if (randomNumber < 500) {
+    standardWinnings = 0;
+  }
+
+  else if (randomNumber < 700  || randomNumber >= 800 && randomNumber < 900) {
+    standardWinnings = standardWinnings * 2;
+  }
+  else if (randomNumber < 800) {
+
+    switch (randomNumber) {
+    case 778:
+    standardWinnings = jackpot / 5;
+    break;
+    case 777:
+    standardWinnings = jackpot;
+    break;
+    case 776:
+    standardWinnings = jackpot / 2;
+    break;
+    default:
+        standardWinnings = standardWinnings * 10;
+  }
+}
+  else
+    {standardWinnings = standardWinnings * 5;
+    }
+
+
+
+  console.log(`Total Winnings: ${standardWinnings}`);
