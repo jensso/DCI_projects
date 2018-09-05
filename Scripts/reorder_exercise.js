e7930cd0bc5ba5f105c9fbf018d7b49ba5e68340
jens soltwedel <jens.soltwedel@hotmail.com>
2018-09-05 20:52:08 +0200

    files renamed, js files edited & readability optimized

--- Scripts/reorder_exercise.js
+++ Scripts/reorder_exercise.js
@@ -1,16 +1,14 @@
-  var randomNumber = 778;
+  var randomNumber = 777;
   var jackpot = 1000;
   var standardWinnings = 10;
 
   if (randomNumber < 500) {
     standardWinnings = 0;
   }
-
   else if (randomNumber < 700  || randomNumber >= 800 && randomNumber < 900) {
     standardWinnings = standardWinnings * 2;
   }
   else if (randomNumber < 800) {
-
     switch (randomNumber) {
     case 778:
     standardWinnings = jackpot / 5;
@@ -22,13 +20,11 @@
     standardWinnings = jackpot / 2;
     break;
     default:
-        standardWinnings = standardWinnings * 10;
+    standardWinnings = standardWinnings * 10;
   }
 }
   else
     {standardWinnings = standardWinnings * 5;
     }
-
-
 
   console.log(`Total Winnings: ${standardWinnings}`);
---
+++ Scripts/switch_example.js
@@ -0,0 +1,19 @@
+
+var anzahlKugeln = 3;
+
+switch (anzahlKugeln) {
+    case 1:
+    console.log(`Sie haben sich für 1 Kugel entschieden. Bitte zahlen Sie 1.00 $.`);
+    break;
+    case 2:
+    console.log(`Sie haben sich für 2 Kugeln entschieden. Bitte zahlen Sie 1.80 $.`);
+    break;
+    case 3:
+    console.log(`Sie haben sich für 3 Kugeln entschieden. Bitte zahlen Sie 2.50 $.`);
+    break;
+    case 4:
+    console.log(`Sie haben sich für 4 Kugeln entschieden. Bitte zahlen Sie 3.30 $.`);
+    break;
+    case 5:
+    console.log(`Sie haben sich für 5 Kugeln entschieden. Bitte zahlen Sie 3.30 $.`);
+}
