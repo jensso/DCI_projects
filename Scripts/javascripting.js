1535647f4f9ab9f20cef14d1165ff9308a40cc14
jens soltwedel <jens.soltwedel@hotmail.com>
2018-09-05 09:11:55 +0200

    js exercises

--- Scripts/javascripting.js
+++ Scripts/javascripting.js
@@ -19,3 +19,43 @@ console.log(365);
 
 // the number of days mercury circles around the sun
 console.log(365*0.15);
+
+console.log('this is my first string');
+console.log('...and this my second');
+console.log(`..maybe I should create a loop.....`);
+
+console.log(`It s been ${2018-1969} years since moon-landing`);
+console.log('It s been '+(2018-1969)+' years since moon-landing');
+
+console.log(typeof(50+`35`));
+console.log(typeof(112+55-`33`));
+
+var myAge = 47;
+var dadsAge = 66+6;
+var dadBecomingDaddy = dadsAge - myAge;
+var vater = `Papa wurde Vater mit ${dadBecomingDaddy}`;
+console.log(vater);
+
+var celsiusReykjavik = 12;
+var celsiusRome = 28;
+var kelvinReykjavik = celsiusReykjavik +273;
+var kelvinRome = celsiusRome +273;
+console.log(kelvinReykjavik);
+console.log(kelvinRome);
+
+var randomNumber = 44;
+var even =  randomNumber  %2;
+
+console.log(even);
+
+var myMoney = 100;
+var hotelRate = 200;
+// assuming a suite cost double as a regular room
+if (myMoney*2 > hotelRate) {
+  console.log(`Take the suite`)
+} else if (myMoney <= hotelRate) {
+  console.log(`take the room`)
+}
+else {
+  console.log(`leave mate!`)
+};
