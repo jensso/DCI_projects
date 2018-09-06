20ef9d16fed33fd1c3b2615b7ddfda0c9aa0889f
jens soltwedel <jens.soltwedel@hotmail.com>
2018-09-06 15:44:54 +0200

    more js exercises

---
+++ Scripts/javascripting.js
@@ -0,0 +1,12 @@
+var randomNumber = 0;
+
+if (randomNumber < 0) {
+  console.log(`${randomNumber} is negative!`);
+}
+  else if (randomNumber > 0) {
+    console.log(`${randomNumber} is positive!`);
+      }
+
+      else {
+        console.log(`${randomNumber} is Zero!`);
+      }
