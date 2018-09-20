c2336a32419e116e98511c4479355d2ea1f9d099
jens soltwedel <jens.soltwedel@hotmail.com>
2018-09-20 09:27:09 +0200

    more js to learn e.g. Objects

--- Scripts/javascripting.js
+++ Scripts/javascripting.js
@@ -1,56 +1,121 @@
-let jake /*object literal*/ = {
-//  keys : values
-  firstName : `Konstantinos`,
-  lastName : `Diakogiannis`,
-  nickName : `Jake`,
-  age : 30,
-  profession : `Machine Learning engineer`,
-  livesAtHome : false,
-};
+// let jake /*object literal*/ = {
+// //  keys : values
+//   firstName : `Konstantinos`,
+//   lastName : `Diakogiannis`,
+//   nickName : `Jake`,
+//   age : 30,
+//   profession : `Machine Learning engineer`,
+//   livesAtHome : false,
+// };
+//
+// console.log(jake.lastName);
+//
+// let jensso = {
+//   firstName : `Jens`,
+//   lastName : `Soltwedel`,
+//   nickName : `jenne`,
+//   age : 47,
+//   profession : `many`,
+//   livesAtHome : true,
+//   changeAge : function(newAge) {
+//     this.age = newAge;
+//   }
+// }
+//
+// // or with different syntax: console.log(jensso[`age`]);
+// console.log(`in 3 years you are going to be ${jensso.age + 3} years old! `);
+//
+// jensso.changeAge(29);
+//
+// console.log(jensso.age);
+//
+//
+// let foreigner = {
+//   firstName : `Pawel`,
+//   lastName : `Mihailowicz`,
+//   age : 27,
+//   yearsOfResidence : 11,
+//   countryOfResidence : `Switzerland`,
+//   countryOfOrigin : `Poland`,
+//   profession : `Facility Manager`,
+//   salaryPerYear : `20000`,
+//   doubleNationality : true,
+//   minimumPresence : 10,
+//   changeNationality : function() {
+//     if (this.yearsOfResidence >= this.minimumPresence) {
+//       if (this.doubleNationality) {
+//       this.countryOfResidence = this.secondNationality;
+//       }
+//       else {
+//         this.countryOfOrigin = this.countryOfResidence;
+//         }
+//       }
+//     }
+//   }
+//
+// foreigner.changeNationality(15);
+// console.log(foreigner.nationality);
+// let LatinInHH = {
+//   nativeTongue : `spanish`,
+//   gender : `male`,
+// }
+//
+// mauro = {
+//   inhProp : Object.create(LatinInHH),
+//   firstName : `Mauro`,
+//   lastName : `Navarro`,
+//   nationality : `chilean`,
+// }
+//
+// marcelo = {
+//   firstName : `Marcelo`,
+//   lastName : `Ramirez`,
+//   nationality : `paraguyan`,
+// }
+//
+// daniel = {
+//   firstName : `Daniel`,
+//   lastName : `Cipolla`,
+//   nationality : `ecuadorian`,
+// }
+//
+// console.log(daniel);
+// console.log(mauro);
+// console.log(marcelo);
 
-console.log(jake.lastName);
-
-let jensso = {
-  firstName : `Jens`,
-  lastName : `Soltwedel`,
-  nickName : `jenne`,
-  age : 47,
-  profession : `many`,
-  livesAtHome : true,
-  changeAge : function(newAge) {
-    this.age = newAge;
-  }
+let moneyWasted = {
+  july : 1005,
+  august : 981,
+  september : 777,
 }
 
-// or with different syntax: console.log(jensso[`age`]);
-console.log(`in 3 years you are going to be ${jensso.age + 3} years old! `);
+calcAverage = function() {
 
-jensso.changeAge(29);
+  let sum = 0;
+for (let property in moneyWasted) {
+  sum += moneyWasted[property];
+}
+return sum / 3;
+}
 
-console.log(jensso.age);
+calcAverage();
+console.log(calcAverage());
 
+let skills = {
+  HtmlSkills : 58,
+  CssSkills : 50,
+  JsSkills : 66,
+}
 
-let foreigner = {
-  firstName : `Pawel`,
-  lastName : `Mihailowicz`,
-  age : 27,
-  yearsOfResidence : 11,
-  nationality : `polish`,
-  profession : `Facility Manager`,
-  salaryPerYear : `20000`,
-  doubleNationality : true,
-  changeNationality : function(threshold) {
-    if (this.yearsOfResidence >= threshold) {
-      if (this.doubleNationality) {
-      nationality = this.secondNationality;
-      }
-      else {
-        foreigner.secondNationality = foreigner.nationality;
-        }
-      }
+calcSkills = function() {
+  let WebDevSkills =0;
+
+  for (let props in skills) {
+    WebDevSkills += skills[props];
     }
-  }
+    return WebDevSkills / 3;
 
+}
 
-foreigner.changeNationality(11);
-console.log(foreigner.nationality);
+calcSkills();
+console.log(calcSkills());
